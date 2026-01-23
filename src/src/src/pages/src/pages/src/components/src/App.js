008327d544b51ad2b961eae5c31b7f9eaf99a164
asadkahloon <asadkahloon506@gmail.com>
2026-01-22 20:53:39 -0800

    Connect pages

---
+++ src/src/src/pages/src/pages/src/components/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import About from './pages/About';
+import Services from './pages/Services';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <About />
+      <Services />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
