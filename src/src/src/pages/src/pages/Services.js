b449c517dd549bd7bd4b04a1969fbaddf6c3ff9d
asadkahloon <asadkahloon506@gmail.com>
2026-01-22 20:50:50 -0800

    Services page

---
+++ src/src/src/pages/src/pages/Services.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Services() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Our Services</h1>
+
+      <ul style={{ lineHeight: 2 }}>
+        <li>Medical Billing & Coding</li>
+        <li>Dental Billing (CDT)</li>
+        <li>Revenue Cycle Management</li>
+        <li>Insurance Verification & Authorizations</li>
+        <li>Denial Management</li>
+        <li>Compliance & Audit Support</li>
+      </ul>
+    </div>
+  );
+}
